92e408aee9d634c54da7227c4745b0acb17226a3
FSVD <fabio.schettino.01@gmail.com>
2017-05-09 22:27:32 +0200

    Error encountered resolving symbol values statically in app-routing.module. SOLVED!
    The solution to previously detected error is deleting all reference to
    InventoryComponent in the router due to a stand-alone module no needs to
    be declared in the router file, only needs to be imported and declared
    in the app.module. All childs of InventoryModule are autoreferenced
    inside the module itself.

--- client-V2/src/app/app-routing.module.ts
+++ client-V2/src/app/app-routing.module.ts
@@ -5,20 +5,20 @@ import { HomeComponent } from './home/home.component';
 import { CustomerComponent } from './customer/customer.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { InventoryModule } from './inventory/inventory.module';
+//import { InventoryModule } from './inventory/inventory.module'; // IMPORTANT! Inventory module import is not necessary from routing module because it is a feature stand-alone module. Only needs import the module from the app.module file.
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'customer', component: CustomerComponent },
     //{ path: 'inventory', loadChildren: () => InventoryModule }, //IMPORTANT! Arrow function does not work with AoT when it is passed to an NgModule.
-    { path: 'inventory', loadChildren: 'app/inventory/inventory.module' },
+    //{ path: 'inventory', loadChildren: 'app/inventory/inventory.module' }, IMPORTANT! Inventory module path is not necessary from routing module because it is a feature stand-alone module. Only needs import the module from the app.module file.
     { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule ({
     imports: [
         RouterModule.forRoot(appRoutes),
-        InventoryModule
+        //InventoryModule IMPORTANT! Inventory module import is not necessary from routing module because it is a feature stand-alone module. Only needs import the module from the app.module file.
     ],
     exports: [
         RouterModule
